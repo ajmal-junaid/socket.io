c1b28de00faf1a5fc0db4861f05d3b785496abe7
Ajmal junaid <ajmal6201@gmail.com>
2023-04-02 21:59:54 +0530

    error fix

--- server/index.js
+++ server/index.js
@@ -46,8 +46,8 @@ io.on("connection", (socket) => {
 
 
 
-server.listen(3000, () => {
-    console.log(
-        "server is running in port 3003"
-    );
-})
+// server.listen(3000, () => {
+//     console.log(
+//         "server is running in port 3003"
+//     );
+// })
