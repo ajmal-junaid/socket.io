f9659ca0b0eda591f5631613b8e07a9ece08af8b
Ajmal junaid <ajmal6201@gmail.com>
2023-04-02 19:54:55 +0530

    io changed

--- server/index.js
+++ server/index.js
@@ -5,26 +5,35 @@ const cors = require('cors')
 const { Server } = require('socket.io')
 app.use(cors());
 
-const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: 'https://my-chat-app-efly.onrender.com',
-        methods: ["GET", "POST"]
-    }
+// const server = http.createServer(app);
+const server = app.listen(PORT, () => {
+    console.log(`Server Running on http://localhost:${PORT}`);
 })
+// const io = new Server(server, {
+//     cors: {
+//         origin: 'https://my-chat-app-efly.onrender.com',
+//         methods: ["GET", "POST"]
+//     }
+// })
+const io = require("socket.io")(server, {
+    // pingTimeout: 60000,
+    cors: {
+        origin: "http://localhost:3000",
+    }
+});
 // origin:"http://localhost:3003"
 
 io.on("connection", (socket) => {
     console.log(`user connected to : ${socket.id}`)
 
-socket.on("join_room",(data)=>{
-    socket.join(data);
-    console.log(`user:  ${socket.id} , joined room :${data}`)
-})
+    socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log(`user:  ${socket.id} , joined room :${data}`)
+    })
 
-socket.on("send_message",(data)=>{
-    socket.to(data.room).emit("recieve_message",data)
-})
+    socket.on("send_message", (data) => {
+        socket.to(data.room).emit("recieve_message", data)
+    })
 
     socket.on("disconnect", () => {
         console.log("user disconnected", socket.id);
