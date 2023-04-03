04a3dc585dec54fda66b5e2755628f418d3e6e95
Ajmal junaid <ajmal6201@gmail.com>
2023-04-03 11:41:00 +0530

    last

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:5173','http://192.168.48.11:3000']
+        origin: ['http://localhost:5173','http://192.168.48.11:3000','https://socket-io-khaki.vercel.app','*']
     }
 })
 // origin:"http://localhost:3003"
